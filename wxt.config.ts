0c8ef23b913aedca59f213cfbc3151dc3a4005dc
Thomas Hartmann <thomashartm@googlemail.com>
2025-06-15 11:41:30 +0200

    Fix wxt config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
+  srcDir: 'src',
   manifestVersion: 3,
   browser: 'chrome',
   targetBrowsers: ['chrome', 'firefox'],
   outDir: 'dist',
-  entrypointsDir: 'src/entrypoints',
+  entrypointsDir: 'entrypoints',
 });
